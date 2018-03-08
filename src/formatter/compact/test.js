4c1f3c88109692cd08e82e0441e0cb1065d2d70c
Alexandre Duarte <duartealexf@gmail.com>
2018-03-08 01:09:21 -0300

    feat: Add ALTER TABLE support for formatter test file.

--- src/formatter/compact/test.js
+++ src/formatter/compact/test.js
@@ -1,7 +1,11 @@
-// const Parser = require('../../../lib');
+const Parser = require('../../../lib');
 const utils = require('../../shared/utils.js');
-const mysqlJson = require('./mysql.json');
+const mysqlJs = require('./mysql');
 const winston = require('winston');
+
+const parser = new Parser();
+parser.feed(mysqlJs);
+const mysqlJson = parser.results;
 
 winston.configure({
   level: 'info',
@@ -39,7 +43,7 @@ const MAIN = def => {
     }
 
     else if (json.def.id === 'P_ALTER_TABLE') {
-      // P_ALTER_TABLE(json.def.def, tables);
+      P_ALTER_TABLE(json.def.def, tables);
     }
 
     // else if (json.def.id === 'P_RENAME_TABLE') {
@@ -57,16 +61,289 @@ const MAIN = def => {
 
 /**
  * Formatter for P_ALTER_TABLE rule's parsed JSON.
+ * Alters one of given existingTables.
  *
+ * @param {any} def Rule's JSON definition.
+ * @param {any[]} existingTables Already existing tables array.
+ * @returns {void}
  */
-// const P_ALTER_TABLE = (def, existingTables) => {
-//   const tableName = def.table;
+const P_ALTER_TABLE = (def, existingTables) => {
+  const tableName = def.table;
+  const table = existingTables.find(t => t.name === def.table);
 
+  if (!table) {
+    logger.warn(`Found "ALTER TABLE" statement for an unexisting table ${tableName}`);
+    return;
+  }
 
-// };
+  table.options = utils.mergeDeep({},
+    table.options,
+    P_CREATE_TABLE_OPTIONS(def.tableOptions)
+  );
+
+  const actionHandlers = {
+    addColumn: json => {
+      const column = {
+        name: json.name,
+        type: O_DATATYPE(json.datatype),
+        options: O_COLUMN_DEFINITION(json.columnDefinition)
+      };
+      if (json.position === null) {
+        table.columns.push(column);
+      }
+      else if (json.position.after === null) {
+        table.columns.unshift(column);
+      }
+      else {
+        const refColumn = table.columns.find(c => c.name === json.position.after);
+
+        if (!refColumn) {
+          logger.warn(`Found "ALTER TABLE [ADD AFTER COLUMN]" statement for an unexisting table column ${json.columns}`);
+          return;
+        }
+
+        const position = table.columns.indexOf(refColumn);
+        const end = table.columns.splice(position + 1);
+        table.columns.push(refColumn);
+        table.columns.concat(end);
+      }
+    },
+    addColumns: json => {
+      table.columns.concat(
+        json.columns.map(column => {
+          return {
+            name: column.name,
+            type: O_DATATYPE(column.def.datatype),
+            options: O_COLUMN_DEFINITION(column.def.columnDefinition)
+          };
+        })
+      );
+    },
+    addIndex: json => {
+      table.indexes.push(
+        {
+          name: json.name,
+          indexType: P_INDEX_TYPE(json.index),
+          columns: P_INDEX_COLUMN(json.columns),
+          options: O_INDEX_OPTION(json.options)
+        }
+      );
+    },
+    addPrimaryKey: json => {
+      if (table.primaryKey) {
+        logger.warn(`Found "ALTER TABLE [ADD PRIMARY KEY]" statement when another primary key already existed.`);
+        return;
+      }
+
+      table.primaryKey = {
+        symbol: json.symbol,
+        indexType: P_INDEX_TYPE(json.index),
+        columns: P_INDEX_COLUMN(json.columns),
+        options: O_INDEX_OPTION(json.options)
+      };
+    },
+    addUniqueKey: json => {
+      table.uniqueKeys.push(
+        {
+          symbol: json.symbol,
+          name: json.name,
+          indexType: P_INDEX_TYPE(json.index),
+          columns: P_INDEX_COLUMN(json.columns),
+          options: O_INDEX_OPTION(json.options)
+        }
+      );
+    },
+    addFulltextIndex: json => {
+      table.fulltextIndexes.push(
+        {
+          name: json.name,
+          columns: P_INDEX_COLUMN(json.columns),
+          options: O_INDEX_OPTION(json.options)
+        }
+      );
+    },
+    addSpatialIndex: json => {
+      table.spatialIndexes.push(
+        {
+          name: json.name,
+          columns: P_INDEX_COLUMN(json.columns),
+          options: O_INDEX_OPTION(json.options)
+        }
+      );
+    },
+    addForeignKey: json => {
+      table.foreignKeys.push(
+        {
+          symbol: json.symbol,
+          name: json.name,
+          columns: P_INDEX_COLUMN(json.columns),
+          reference: P_COLUMN_REFERENCE(json.reference.def)
+        }
+      );
+    },
+    // changeAlgorithm: json => {
+    // },
+    setDefaultColumnValue: json => {
+      const column = table.find(c => c.name === json.column);
+
+      if (!column) {
+        logger.warn(`Found "ALTER TABLE [SET DEFAULT COLUMN VALUE]" statement for an unexisting table column ${json.column}`);
+        return;
+      }
+
+      column.options.default = json.value;
+    },
+    dropDefaultColumnValue: json => {
+      const column = table.find(c => c.name === json.column);
+
+      if (!column) {
+        logger.warn(`Found "ALTER TABLE [DROP DEFAULT COLUMN VALUE]" statement for an unexisting table column ${json.column}`);
+        return;
+      }
+
+      delete column.options.default;
+    },
+    changeColumn: json => {
+      const column = table.find(c => c.name === json.column);
+
+      if (!column) {
+        logger.warn(`Found "ALTER TABLE [CHANGE/MODIFY COLUMN]" statement for an unexisting table column ${json.column}`);
+        return;
+      }
+
+      if (json.newName) {
+        column.name = json.newName;
+      }
+
+      column.type = O_DATATYPE(json.datatype);
+
+      if (json.columnDefinition.length) {
+        column.options = utils.mergeDeep({},
+          column.options,
+          O_COLUMN_DEFINITION(json.columnDefinition)
+        );
+      }
+
+      if (json.position) {
+        if (json.position.after === null) {
+          table.columns.unshift(column);
+        }
+        else {
+          const refColumn = table.columns.find(c => c.name === json.position.after);
+
+          if (!refColumn) {
+            logger.warn(`Found "ALTER TABLE [CHANGE/MODIFY COLUMN POSITION]" statement for an unexisting table column ${json.column}`);
+            return;
+          }
+
+          const position = table.columns.indexOf(refColumn);
+          const end = table.columns.splice(position + 1);
+          table.columns.push(refColumn);
+          table.columns.concat(end);
+        }
+      }
+    },
+    changeCharacterSet: json => {
+      table.options.charset = json.charset;
+
+      if (json.collate) {
+        table.options.collate = json.collate;
+      }
+    },
+    // convertToCharacterSet: json => {
+    // },
+    // enableKeys: json => {
+    // },
+    // disableKeys: json => {
+    // },
+    // discardTablespace: json => {
+    // },
+    // importTablespace: json => {
+    // },
+    dropColumn: json => {
+      const column = table.columns.find(c => c.name === json.column);
+
+      if (!column) {
+        logger.warn(`Found "ALTER TABLE [DROP COLUMN]" statement for an unexisting table column ${json.column}`);
+        return;
+      }
+
+      const end = table.columns.splice(
+        table.columns.indexOf(column)
+      );
+      end.shift();
+      table.columns.concat(end);
+    },
+    dropIndex: json => {
+      const index = table.indexes.find(i => i.name === json.index);
+
+      if (!index) {
+        logger.warn(`Found "ALTER TABLE [DROP INDEX]" statement for an unexisting table index ${json.index}`);
+        return;
+      }
+
+      const end = table.indexes.splice(
+        table.indexes.indexOf(index)
+      );
+      end.shift();
+      table.indexes.concat(end);
+    },
+    dropPrimaryKey: () => {
+      delete table.primaryKey;
+    },
+    dropForeignKey: json => {
+      const key = table.foreignKeys.find(k => k.symbol === json.key);
+
+      if (!key) {
+        logger.warn(`Found "ALTER TABLE [DROP FOREIGN KEY]" statement referencing unexisting key ${json.key}`);
+        return;
+      }
+
+      const end = table.foreignKeys.splice(
+        table.foreignKeys.indexOf(key)
+      );
+      end.shift();
+      table.foreignKeys.concat(end);
+    },
+    // force: json => {
+    // },
+    // changeLock: json => {
+    // },
+    // orderBy: json => {
+    // },
+    renameIndex: json => {
+      const index = table.indexes.find(i => i.name === json.index);
+
+      if (!index) {
+        logger.warn(`Found "ALTER TABLE [RENAME INDEX]" statement for an unexisting table index ${json.index}`);
+        return;
+      }
+
+      index.name = json.newName;
+    },
+    rename: json => {
+      table.name = json.newName;
+    },
+    // withValidation: json => {
+    // },
+    // withoutValidation: json => {
+    // },
+    // addPeriodForSystemTime: json => {
+    // }
+  };
+
+  def.specs.forEach(spec => {
+    actionHandlers[spec.def.spec.def.action](spec.def.spec.def);
+    table.options = utils.mergeDeep({},
+      table.options,
+      P_CREATE_TABLE_OPTIONS(spec.def.tableOptions)
+    );
+  });
+};
 
 /**
  * Formatter for P_CREATE_INDEX rule's parsed JSON.
+ * Alters one of given existingTables.
  *
  * @param {any} def Rule's JSON definition.
  * @param {any[]} existingTables Already existing tables array.
@@ -128,26 +405,70 @@ const P_CREATE_INDEX = (def, existingTables) => {
  * Formatter for P_CREATE_TABLE_COMMON rule's parsed JSON.
  *
  * @param {any} def Rule's JSON definition.
- * @returns {any} Table definition.
+ * @returns {any} Created table definition.
  */
 const P_CREATE_TABLE_COMMON = def => {
-  const table = {};
+  const table = {
+    columns: [],
+    fulltextIndexes: [],
+    spatialIndexes: [],
+    foreignKeys: [],
+    uniqueKeys: [],
+    indexes: []
+  };
 
   const columns         = def.columnsDef.def.filter(json => utils.isDefined(json.def.column));
   const fulltextIndexes = def.columnsDef.def.filter(json => utils.isDefined(json.def.fulltextIndex));
   const spatialIndexes  = def.columnsDef.def.filter(json => utils.isDefined(json.def.spatialIndex));
   const foreignKeys     = def.columnsDef.def.filter(json => utils.isDefined(json.def.foreignKey));
-  const primaryKeys     = def.columnsDef.def.filter(json => utils.isDefined(json.def.primaryKey));
   const uniqueKeys      = def.columnsDef.def.filter(json => utils.isDefined(json.def.uniqueKey));
   const indexes         = def.columnsDef.def.filter(json => utils.isDefined(json.def.index));
 
+  const primaryKey      = def.columnsDef.def.find(json   => utils.isDefined(json.def.primaryKey));
+
   table.columns = columns.map(json => {
-    const column = json.def.column;
-    return {
+    let column = json.def.column;
+    column = {
       name: column.name,
       type: O_DATATYPE(column.def.datatype),
       options: O_COLUMN_DEFINITION(column.def.columnDefinition)
     };
+
+    if (utils.isDefined(column.options.unique)) {
+      table.uniqueKeys.push({
+        symbol: null,
+        name: null,
+        indexType: null,
+        columns: [{ column: column.name, length: null, sort: null }],
+        options: null
+      });
+
+      delete column.options.unique;
+    }
+
+    if (utils.isDefined(column.options.primary)) {
+      table.primaryKey = {
+        symbol: null,
+        indexType: null,
+        columns: [{ column: column.name, length: null, sort: null }],
+        options: null
+      };
+
+      delete column.options.primary;
+    }
+
+    if (utils.isDefined(column.options.reference)) {
+      table.foreignKeys.push({
+        symbol: null,
+        name: null,
+        columns: [{ column: column.name, length: null, sort: null }],
+        reference: P_COLUMN_REFERENCE(column.options.reference.def)
+      });
+
+      delete column.options.reference;
+    }
+
+    return column;
   });
 
   table.fulltextIndexes = fulltextIndexes.map(json => {
@@ -178,15 +499,19 @@ const P_CREATE_TABLE_COMMON = def => {
     };
   });
 
-  table.primaryKeys = primaryKeys.map(json => {
-    const primaryKey = json.def.primaryKey;
-    return {
-      symbol: primaryKey.symbol,
-      indexType: P_INDEX_TYPE(primaryKey.index),
-      columns: P_INDEX_COLUMN(primaryKey.columns),
-      options: O_INDEX_OPTION(primaryKey.options)
-    };
-  });
+  if (primaryKey) {
+    if (table.primaryKey) {
+      logger.warn(`Found "CREATE TABLE" adding a primary key, when another primary key already existed.`);
+    }
+    else {
+      table.primaryKey = {
+        symbol: primaryKey.def.primaryKey.symbol,
+        indexType: P_INDEX_TYPE(primaryKey.def.primaryKey.index),
+        columns: P_INDEX_COLUMN(primaryKey.def.primaryKey.columns),
+        options: O_INDEX_OPTION(primaryKey.def.primaryKey.options)
+      };
+    }
+  }
 
   table.uniqueKeys = uniqueKeys.map(json => {
     const uniqueKey = json.def.uniqueKey;
@@ -224,8 +549,12 @@ const P_CREATE_TABLE_COMMON = def => {
  * @returns {any} Single object with all options.
  */
 const P_CREATE_TABLE_OPTIONS = def => {
+  if (!def) {
+    return {};
+  }
+
   return utils.mergeLatestToObject(
-    def.map(json => json.def)
+    def.def.map(json => json.def)
   );
 };
 

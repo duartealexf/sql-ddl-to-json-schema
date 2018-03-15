baad5a65ccb4cdda3cafc8630f5ccf6411a42b0c
Alexandre Duarte <duartealexf@gmail.com>
2018-03-15 18:22:53 -0300

    test: Add stubs for five ALTER TABLE formatter tests.

---
+++ test/mysql/formatter/compact/alter-table-add-index.spec.js
@@ -0,0 +1,10 @@
+const ava = require('ava');
+const fs = require('fs');
+const path = require('path');
+
+const Parser = require('../../../../lib');
+const expect = require('./expect/alter-table-add-index.json');
+
+const sql = fs.readFileSync(path.join(__dirname, 'sql', 'create-table.sql')).toString();
+
+ava.todo('Will test ALTER TABLE ADD INDEX.');

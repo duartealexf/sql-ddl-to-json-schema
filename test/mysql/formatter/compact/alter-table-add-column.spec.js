be9261dbd2fb3a68b64ded27b0decc879b15fe48
Alexandre Duarte <duartealexf@gmail.com>
2018-03-15 22:42:03 -0300

    test: Add compact formatter for ALTER TABLE ADD COLUMN.

--- test/mysql/formatter/compact/alter-table-add-column.spec.js
+++ test/mysql/formatter/compact/alter-table-add-column.spec.js
@@ -7,4 +7,15 @@ const expect = require('./expect/alter-table-add-column.json');
 
 const sql = fs.readFileSync(path.join(__dirname, 'sql', 'create-table.sql')).toString();
 
-ava.todo('Will test ALTER TABLE ADD COLUMN.');
+ava('Compact formatter: Should alter table, adding columns in different positions.', t => {
+  const parser = new Parser('mysql');
+  parser.feed(sql);
+
+  parser.feed('ALTER TABLE person ADD is_married BOOLEAN AFTER suffix;');
+  parser.feed('ALTER TABLE person ADD uuid VARCHAR(36) FIRST;');
+  parser.feed('ALTER TABLE person ADD updated_at TIMESTAMP;');
+
+  const json = parser.toCompactJson(parser.results);
+  // for some reason t.deepEqual hangs process
+  t.is(JSON.stringify(json), JSON.stringify(expect));
+});

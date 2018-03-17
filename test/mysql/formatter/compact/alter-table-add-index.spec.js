0ff34e80b73f91346df19fdbd82440fc9db645fa
Alexandre Duarte <duartealexf@gmail.com>
2018-03-17 20:27:39 -0300

    test: Add formatter test for ALTER TABLE ADD INDEX statement.

--- test/mysql/formatter/compact/alter-table-add-index.spec.js
+++ test/mysql/formatter/compact/alter-table-add-index.spec.js
@@ -7,4 +7,15 @@ const expect = require('./expect/alter-table-add-index.json');
 
 const sql = fs.readFileSync(path.join(__dirname, 'sql', 'create-table.sql')).toString();
 
-ava.todo('Will test ALTER TABLE ADD INDEX.');
+ava('Compact formatter: Should alter table, adding index.', t => {
+  const parser = new Parser('mysql');
+  parser.feed(sql);
+
+  parser.feed('ALTER TABLE house ADD INDEX idx_built (is_built (1) asc, is_apartment (2)) KEY_BLOCK_SIZE = 16 USING BTREE WITH PARSER myParser COMMENT "formatter test";');
+  parser.feed('ALTER TABLE house ADD KEY (where);');
+
+  const json = parser.toCompactJson(parser.results);
+  // for some reason t.deepEqual hangs process
+  t.is(JSON.stringify(json), JSON.stringify(expect));
+});
+

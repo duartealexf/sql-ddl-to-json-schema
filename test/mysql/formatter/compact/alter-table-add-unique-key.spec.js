24d384e782d7c14c78fb6f129f221e1c4a151505
Alexandre Duarte <duartealexf@gmail.com>
2018-03-17 20:55:12 -0300

    test: Add formatter test for ALTER TABLE ADD UNIQUE KEY statement.

--- test/mysql/formatter/compact/alter-table-add-unique-key.spec.js
+++ test/mysql/formatter/compact/alter-table-add-unique-key.spec.js
@@ -7,4 +7,15 @@ const expect = require('./expect/alter-table-add-unique-key.json');
 
 const sql = fs.readFileSync(path.join(__dirname, 'sql', 'create-table.sql')).toString();
 
-ava.todo('Will test ALTER TABLE ADD UNIQUE KEY.');
+ava('Compact formatter: Should alter table, adding unique key.', t => {
+  const parser = new Parser('mysql');
+  parser.feed(sql);
+
+  parser.feed('ALTER TABLE pet ADD CONSTRAINT uq_birth UNIQUE KEY unique_birth (birth(12) desc, status(1)) KEY_BLOCK_SIZE 64 USING BTREE WITH PARSER uniqueParser COMMENT "unique formatter test";');
+  parser.feed('ALTER TABLE pet ADD CONSTRAINT UNIQUE INDEX (history);');
+  parser.feed('ALTER TABLE pet ADD UNIQUE KEY INDEX (shape);');
+
+  const json = parser.toCompactJson(parser.results);
+  // for some reason t.deepEqual hangs process
+  t.is(JSON.stringify(json), JSON.stringify(expect));
+});

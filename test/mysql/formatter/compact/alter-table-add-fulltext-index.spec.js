6da45eac1385a3efc42d8139f44c9f0e52eae555
Alexandre Duarte <duartealexf@gmail.com>
2018-03-17 20:18:17 -0300

    test: Add formatter test for ALTER TABLE ADD FULLTEXT INDEX statement.

--- test/mysql/formatter/compact/alter-table-add-fulltext-index.spec.js
+++ test/mysql/formatter/compact/alter-table-add-fulltext-index.spec.js
@@ -7,4 +7,15 @@ const expect = require('./expect/alter-table-add-fulltext-index.json');
 
 const sql = fs.readFileSync(path.join(__dirname, 'sql', 'create-table.sql')).toString();
 
-ava.todo('Will test ALTER TABLE ADD FULLTEXT INDEX.');
+ava('Compact formatter: Should alter table, adding fulltext index.', t => {
+  const parser = new Parser('mysql');
+  parser.feed(sql);
+
+  parser.feed('ALTER TABLE house ADD FULLTEXT INDEX fi_history (history (200) desc, photo (100)) KEY_BLOCK_SIZE = 1024 USING HASH WITH PARSER myParser COMMENT "formatter test";');
+  parser.feed('ALTER TABLE house ADD FULLTEXT KEY (letter);');
+
+  const json = parser.toCompactJson(parser.results);
+  // for some reason t.deepEqual hangs process
+  t.is(JSON.stringify(json), JSON.stringify(expect));
+});
+

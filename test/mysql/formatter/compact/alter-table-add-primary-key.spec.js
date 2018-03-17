6266df8d9f7716fe552655881348358b61cbc126
Alexandre Duarte <duartealexf@gmail.com>
2018-03-17 20:34:26 -0300

    test: Add formatter test for ALTER TABLE ADD SPATIAL INDEX statement.

--- test/mysql/formatter/compact/alter-table-add-primary-key.spec.js
+++ test/mysql/formatter/compact/alter-table-add-primary-key.spec.js
@@ -7,4 +7,14 @@ const expect = require('./expect/alter-table-add-primary-key.json');
 
 const sql = fs.readFileSync(path.join(__dirname, 'sql', 'create-table.sql')).toString();
 
-ava.todo('Will test ALTER TABLE ADD PRIMARY KEY.');
+ava('Compact formatter: Should alter table, adding primary key.', t => {
+  const parser = new Parser('mysql');
+  parser.feed(sql);
+
+  parser.feed('ALTER TABLE pet ADD CONSTRAINT pk_id PRIMARY KEY USING RTREE (id(6) asc, species(100)) KEY_BLOCK_SIZE 512 USING HASH WITH PARSER myParser COMMENT "pk formatter test";');
+  parser.feed('ALTER TABLE person ADD PRIMARY KEY (ssn);');
+
+  const json = parser.toCompactJson(parser.results);
+  // for some reason t.deepEqual hangs process
+  t.is(JSON.stringify(json), JSON.stringify(expect));
+});

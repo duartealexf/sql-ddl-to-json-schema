00418f3e96f25c51af1e930aba5e2b0f16d6890d
Alexandre Duarte <duartealexf@gmail.com>
2018-03-17 20:43:43 -0300

    test(Add formatter test for ALTER TABLE ADD SPATIAL INDEX statement.):

--- test/mysql/formatter/compact/alter-table-add-spatial-index.spec.js
+++ test/mysql/formatter/compact/alter-table-add-spatial-index.spec.js
@@ -7,4 +7,14 @@ const expect = require('./expect/alter-table-add-spatial-index.json');
 
 const sql = fs.readFileSync(path.join(__dirname, 'sql', 'create-table.sql')).toString();
 
-ava.todo('Will test ALTER TABLE ADD SPATIAL INDEX.');
+ava('Compact formatter: Should alter table, adding spatial index.', t => {
+  const parser = new Parser('mysql');
+  parser.feed(sql);
+
+  parser.feed('ALTER TABLE person ADD SPATIAL INDEX si_pattern (pattern(21) asc, shape(10)) KEY_BLOCK_SIZE 2048 USING HASH WITH PARSER spatialParser COMMENT "spatial formatter test";');
+  parser.feed('ALTER TABLE pet ADD SPATIAL INDEX (shape);');
+
+  const json = parser.toCompactJson(parser.results);
+  // for some reason t.deepEqual hangs process
+  t.is(JSON.stringify(json), JSON.stringify(expect));
+});

7d45aea09c177a5128cf991ac83ca37ad711f63d
Alexandre Duarte <duartealexf@gmail.com>
2018-03-18 09:40:17 -0300

    test: Add formatter test for ALTER TABLE SET DEFAULT COLUMN VALUE statement.

--- test/mysql/formatter/compact/alter-table-set-default-col-value.spec.js
+++ test/mysql/formatter/compact/alter-table-set-default-col-value.spec.js
@@ -7,4 +7,14 @@ const expect = require('./expect/alter-table-set-default-col-value.json');
 
 const sql = fs.readFileSync(path.join(__dirname, 'sql', 'create-table.sql')).toString();
 
-ava.todo('Will test ALTER TABLE SET DEFAULT COLUMN VALUE.');
+ava('Compact formatter: Should alter table, renaming it.', t => {
+  const parser = new Parser('mysql');
+  parser.feed(sql);
+
+  parser.feed('ALTER TABLE person ALTER COLUMN status SET DEFAULT 0;');
+  parser.feed('ALTER TABLE person ALTER nickname SET DEFAULT "JJ";');
+
+  const json = parser.toCompactJson(parser.results);
+  // for some reason t.deepEqual hangs process
+  t.is(JSON.stringify(json), JSON.stringify(expect));
+});

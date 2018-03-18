a1e638a14bda2c25fa3f36efc66bd70d8b9f299f
Alexandre Duarte <duartealexf@gmail.com>
2018-03-18 09:30:25 -0300

    test: Add formatter test for ALTER TABLE RENAME TO statement.

--- test/mysql/formatter/compact/alter-table-rename-table.spec.js
+++ test/mysql/formatter/compact/alter-table-rename-table.spec.js
@@ -7,4 +7,14 @@ const expect = require('./expect/alter-table-rename-table.json');
 
 const sql = fs.readFileSync(path.join(__dirname, 'sql', 'create-table.sql')).toString();
 
-ava.todo('Will test ALTER TABLE RENAME TO.');
+ava('Compact formatter: Should alter table, renaming it.', t => {
+  const parser = new Parser('mysql');
+  parser.feed(sql);
+
+  parser.feed('ALTER TABLE person RENAME TO people;');
+  parser.feed('ALTER TABLE pet RENAME AS dog;');
+
+  const json = parser.toCompactJson(parser.results);
+  // for some reason t.deepEqual hangs process
+  t.is(JSON.stringify(json), JSON.stringify(expect));
+});

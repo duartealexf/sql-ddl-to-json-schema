613047730483a6910c21020b682516ff41f11892
Alexandre Duarte <duartealexf@gmail.com>
2018-03-17 23:08:22 -0300

    test: Add formatter test for ALTER TABLE DROP PRIMARY KEY statement.

--- test/mysql/formatter/compact/alter-table-drop-primary-key.spec.js
+++ test/mysql/formatter/compact/alter-table-drop-primary-key.spec.js
@@ -7,4 +7,13 @@ const expect = require('./expect/alter-table-drop-primary-key.json');
 
 const sql = fs.readFileSync(path.join(__dirname, 'sql', 'create-table.sql')).toString();
 
-ava.todo('Will test ALTER TABLE DROP PRIMARY KEY.');
+ava('Compact formatter: Should alter table, dropping primary key.', t => {
+  const parser = new Parser('mysql');
+  parser.feed(sql);
+
+  parser.feed('ALTER TABLE person DROP PRIMARY KEY;');
+
+  const json = parser.toCompactJson(parser.results);
+  // for some reason t.deepEqual hangs process
+  t.is(JSON.stringify(json), JSON.stringify(expect));
+});

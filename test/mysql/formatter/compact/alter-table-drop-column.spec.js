f39c5ca6af4632f251637b632d4198a710686ebc
Alexandre Duarte <duartealexf@gmail.com>
2018-03-17 22:33:50 -0300

    test: Add formatter test for ALTER TABLE DROP COLUMN statement.

--- test/mysql/formatter/compact/alter-table-drop-column.spec.js
+++ test/mysql/formatter/compact/alter-table-drop-column.spec.js
@@ -7,4 +7,13 @@ const expect = require('./expect/alter-table-drop-column.json');
 
 const sql = fs.readFileSync(path.join(__dirname, 'sql', 'create-table.sql')).toString();
 
-ava.todo('Will test ALTER TABLE DROP COLUMN.');
+ava('Compact formatter: Should alter table, dropping column.', t => {
+  const parser = new Parser('mysql');
+  parser.feed(sql);
+
+  parser.feed('ALTER TABLE person DROP COLUMN tob;');
+
+  const json = parser.toCompactJson(parser.results);
+  // for some reason t.deepEqual hangs process
+  t.is(JSON.stringify(json), JSON.stringify(expect));
+});

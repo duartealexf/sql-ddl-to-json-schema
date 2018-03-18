1d29dffed39a64860ddd094f901d4deaddfe839f
Alexandre Duarte <duartealexf@gmail.com>
2018-03-17 22:40:48 -0300

    test: Add formatter test for ALTER TABLE ALTER COLUMN DROP DEFAULT statement.

--- test/mysql/formatter/compact/alter-table-drop-default-col-value.spec.js
+++ test/mysql/formatter/compact/alter-table-drop-default-col-value.spec.js
@@ -7,4 +7,13 @@ const expect = require('./expect/alter-table-drop-default-col-value.json');
 
 const sql = fs.readFileSync(path.join(__dirname, 'sql', 'create-table.sql')).toString();
 
-ava.todo('Will test ALTER TABLE DROP DEFAULT COLUMN VALUE.');
+ava('Compact formatter: Should alter table, dropping default column value.', t => {
+  const parser = new Parser('mysql');
+  parser.feed(sql);
+
+  parser.feed('ALTER TABLE person ALTER COLUMN nickname DROP DEFAULT;');
+
+  const json = parser.toCompactJson(parser.results);
+  // for some reason t.deepEqual hangs process
+  t.is(JSON.stringify(json), JSON.stringify(expect));
+});

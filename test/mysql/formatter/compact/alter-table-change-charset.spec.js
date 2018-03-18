b8bbd0a7f194d6e7df3f7908d7bbdcbf3fd71f06
Alexandre Duarte <duartealexf@gmail.com>
2018-03-17 21:04:06 -0300

    test: Add formatter test for ALTER TABLE CHARACTER SET statement.

--- test/mysql/formatter/compact/alter-table-change-charset.spec.js
+++ test/mysql/formatter/compact/alter-table-change-charset.spec.js
@@ -7,4 +7,14 @@ const expect = require('./expect/alter-table-change-charset.json');
 
 const sql = fs.readFileSync(path.join(__dirname, 'sql', 'create-table.sql')).toString();
 
-ava.todo('Will test ALTER TABLE CHANGE CHARACTER SET / COLLATE.');
+ava('Compact formatter: Should alter table, changing charset.', t => {
+  const parser = new Parser('mysql');
+  parser.feed(sql);
+
+  parser.feed('ALTER TABLE person DEFAULT CHARACTER SET latin1 COLLATE = latin1_general_ci;');
+  parser.feed('ALTER TABLE pet CHARACTER SET pet_charset;');
+
+  const json = parser.toCompactJson(parser.results);
+  // for some reason t.deepEqual hangs process
+  t.is(JSON.stringify(json), JSON.stringify(expect));
+});

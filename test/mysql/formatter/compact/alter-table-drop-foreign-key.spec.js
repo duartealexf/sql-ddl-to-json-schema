ff89263b54e1100abdbd7ddceb9dfbb1d4caac67
Alexandre Duarte <duartealexf@gmail.com>
2018-03-17 22:43:40 -0300

    test: Add formatter test for ALTER TABLE DROP FOREIGN KEY statement.

--- test/mysql/formatter/compact/alter-table-drop-foreign-key.spec.js
+++ test/mysql/formatter/compact/alter-table-drop-foreign-key.spec.js
@@ -7,4 +7,13 @@ const expect = require('./expect/alter-table-drop-foreign-key.json');
 
 const sql = fs.readFileSync(path.join(__dirname, 'sql', 'create-table.sql')).toString();
 
-ava.todo('Will test ALTER TABLE DROP FOREIGN KEY.');
+ava('Compact formatter: Should alter table, dropping foreign key.', t => {
+  const parser = new Parser('mysql');
+  parser.feed(sql);
+
+  parser.feed('ALTER TABLE house DROP FOREIGN KEY fk_pet_id;');
+
+  const json = parser.toCompactJson(parser.results);
+  // for some reason t.deepEqual hangs process
+  t.is(JSON.stringify(json), JSON.stringify(expect));
+});

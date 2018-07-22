7a7adae38fb5b4502d5a62dd475c5cadb8129920
Alexandre Duarte <duartealexf@gmail.com>
2018-07-22 00:58:24 -0300

    test: Fix JSON Schema formatter test.

--- test/mysql/formatter/json-schema/formatter.spec.js
+++ test/mysql/formatter/json-schema/formatter.spec.js
@@ -20,13 +20,13 @@ ava('JSON Schema formatter: should format array elements correctly.', t => {
   const personSchema = jsonSchemas.find(s => s.$id === 'person');
   const petSchema = jsonSchemas.find(s => s.$id === 'pet');
   const houseSchema = jsonSchemas.find(s => s.$id === 'house');
-  fs.writeFileSync(path.join(__dirname, 'expect', 'person.json'), JSON.stringify(personSchema, null, 2));
-  fs.writeFileSync(path.join(__dirname, 'expect', 'pet.json'), JSON.stringify(petSchema, null, 2));
-  fs.writeFileSync(path.join(__dirname, 'expect', 'house.json'), JSON.stringify(houseSchema, null, 2));
+  // fs.writeFileSync(path.join(__dirname, 'expect', 'person.json'), JSON.stringify(personSchema, null, 2));
+  // fs.writeFileSync(path.join(__dirname, 'expect', 'pet.json'), JSON.stringify(petSchema, null, 2));
+  // fs.writeFileSync(path.join(__dirname, 'expect', 'house.json'), JSON.stringify(houseSchema, null, 2));
   // for some reason t.deepEqual hangs process
-  // t.is(JSON.stringify(personSchema), JSON.stringify(personTable));
-  // t.is(JSON.stringify(petSchema), JSON.stringify(petTable));
-  // t.is(JSON.stringify(houseSchema), JSON.stringify(houseTable));
-  t.pass();
+  t.is(JSON.stringify(personSchema), JSON.stringify(personTable));
+  t.is(JSON.stringify(petSchema), JSON.stringify(petTable));
+  t.is(JSON.stringify(houseSchema), JSON.stringify(houseTable));
+  // t.pass();
 });
 
